3f5271b4ecff31d3580b94c35cae7875da34e443
bent gunnar <bent@franklyinc.com>
2017-09-20 19:04:59 +0000

    hello

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ class RandomTest extends Component {
   render(){
     let {
       //h1 = dep(),
-      h2 = 'KOTV FOOTER TEST'
+      h2 = 'KOTV FOOTER TEST. hello'
     } = this.props;
 
     let {
